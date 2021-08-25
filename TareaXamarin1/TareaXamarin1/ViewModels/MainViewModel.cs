8fdd39cf1d876f2ddd3a2e9b6d9afc5f514b6925
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2021-08-25 00:51:18 -0400

    Cambiada la logica de la tarea 1 de code behind a MVVM

---
+++ TareaXamarin1/TareaXamarin1/ViewModels/MainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TareaXamarin1.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private string _input_user;
+        private string _input_password;
+
+        public string InputUser 
+        {
+            get { return _input_user; }
+            set {
+
+                _input_user = value;
+                OnPropertyChanged(nameof(InputUser));
+            
+            } 
+        }
+        public string InputPassword
+        {
+            get { return _input_password; }
+            set
+            {
+
+                _input_password = value;
+                OnPropertyChanged(nameof(InputPassword));
+
+            }
+        }
+
+        async private void Login()
+        {
+            if (string.IsNullOrEmpty(InputPassword) && string.IsNullOrEmpty(InputUser))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Por favor introduzca sus credenciales ", "Ok");
+
+            }
+            else if (string.IsNullOrEmpty(InputPassword))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Por favor introduzca su contrasena ", "Ok");
+            }
+            else if (string.IsNullOrEmpty(InputUser))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Por favor introduzca su nombre de usuario ", "Ok");
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Bienvenido", $"Hola, {InputUser}", "Ok");
+            }
+        }
+
+        public ICommand LoginComand{ get; }
+
+        public MainViewModel()
+        {
+            LoginComand = new Command(Login);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));  
+        }
+    }
+}
---
+++ TareaXamarin1/TareaXamarin1/Views/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TareaXamarin1.ViewModels;
+using Xamarin.Forms;
+
+namespace TareaXamarin1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
