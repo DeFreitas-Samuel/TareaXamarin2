82c485d367d786227dfec72f3256e89095422750
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2021-08-27 14:00:05 -0400

    Bindeadas tdas las propiedades a sus respectivos view elements

--- TareaXamarin1/TareaXamarin1/Models/MenuOption.cs
+++ TareaXamarin1/TareaXamarin1/Models/MenuOption.cs
@@ -5,12 +5,11 @@ using System.Text;
 
 namespace TareaXamarin1.Models
 {
-    public class MenuOption : INotifyPropertyChanged
+    public class MenuOption 
     {
         public string Title { get; set; }
         public string Image { get; set; }
 
-        public event PropertyChangedEventHandler PropertyChanged;
 
     }
 }
--- TareaXamarin1/TareaXamarin1/ViewModels/DiscoveryViewModel.cs
+++ TareaXamarin1/TareaXamarin1/ViewModels/DiscoveryViewModel.cs
@@ -6,18 +6,34 @@ using TareaXamarin1.Models;
 
 namespace TareaXamarin1.ViewModels
 {
-    class DiscoveryViewModel : INotifyPropertyChanged
+    class DiscoveryViewModel
     {
 
 
         public MenuOption BarAndHotelsMenuOption { get; set; } = new MenuOption();
+        public MenuOption FineDiningMenuOption { get; set; } = new MenuOption();
+        public MenuOption CafesMenuOption { get; set; } = new MenuOption();
+        public MenuOption NearbyMenuOption { get; set; } = new MenuOption();
+        public MenuOption FastFoodsMenuOption { get; set; } = new MenuOption();
+        public MenuOption FeaturedFoodMenuOption { get; set; } = new MenuOption();
+        
         public DiscoveryViewModel()
         {
-            BarAndHotelsMenuOption.Title = "Bars &amp; Hotels";
+            BarAndHotelsMenuOption.Title = "Bars & Hotels";
             BarAndHotelsMenuOption.Image = "bars";
+            FineDiningMenuOption.Title = "Fine Dining";
+            FineDiningMenuOption.Image = "fine_dining";
+            CafesMenuOption.Title = "Cafes";
+            CafesMenuOption.Image = "cafes";
+            NearbyMenuOption.Title = "Nearby";
+            NearbyMenuOption.Image = "nearby";
+            FastFoodsMenuOption.Title = "Fast Foods";
+            FastFoodsMenuOption.Image = "fast_food";
+            FeaturedFoodMenuOption.Title = "Featured Food";
+            FeaturedFoodMenuOption.Image = "featured_food";
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        
 
     }
 }
